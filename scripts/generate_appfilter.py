5589a5be885642aeb4408d91785cf6b963cadae6
Alexej Tušl <72282274+xefensor@users.noreply.github.com>
2026-08-11 13:45:01 +0200

    Fix appfilter generator defaults

--- scripts/generate_appfilter.py
+++ scripts/generate_appfilter.py
@@ -1,23 +1,25 @@
 import json
-from os import environ, path, remove
+from os import environ, makedirs, path, remove
 
 ABS_PATH = path.dirname(path.abspath(__file__))
 
-DB_FILE = environ.get('DB_FILE', path.join(ABS_PATH, 'data.json'))
-APPFILTER_FILE = environ.get('APPFILTER_FILE', path.join(ABS_PATH, '../app/src/main/res/xml/appfilter.xml'))
+DB_FILE = environ.get('DB_FILE', path.join(ABS_PATH, '../data.json'))
+APPFILTER_FILE = environ.get(
+    'APPFILTER_FILE',
+    path.join(ABS_PATH, '../app/src/main/res/xml/appfilter.xml'),
+)
 
+makedirs(path.dirname(path.abspath(APPFILTER_FILE)), exist_ok=True)
 
-# Remove the existing appfilter.xml file
 if path.exists(APPFILTER_FILE):
     remove(APPFILTER_FILE)
 
-# Read the database
-with open(DB_FILE, 'r') as db_obj:
+with open(DB_FILE, 'r', encoding='utf-8') as db_obj:
     try:
         data = json.load(db_obj)
     except ValueError:
         print("Invalid JSON database file")
-        exit(0)
+        raise SystemExit(1)
 
 app_filter_content = '<?xml version="1.0" encoding="utf-8"?>\n'
 app_filter_content += '<resources>\n'
@@ -25,12 +27,16 @@ app_filter_content += '<resources>\n'
 for icon_src, components in data.items():
     drawable_name = path.basename(icon_src)
     for component_info in components:
-        app_filter_content += '\t<item component="ComponentInfo{' + \
-            component_info + '}" drawable="' + drawable_name + '"/>\n'
+        app_filter_content += (
+            '\t<item component="ComponentInfo{'
+            + component_info
+            + '}" drawable="'
+            + drawable_name
+            + '"/>\n'
+        )
 
 app_filter_content += '</resources>'
-with open(APPFILTER_FILE, 'w') as app_filter_obj:
+with open(APPFILTER_FILE, 'w', encoding='utf-8') as app_filter_obj:
     app_filter_obj.write(app_filter_content)
 
 print("Generated {} with {} icons.".format(APPFILTER_FILE, len(data)))
-
